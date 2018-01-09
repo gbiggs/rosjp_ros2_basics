c8affbed0495594c52f10e61185e76362a41377c
Geoffrey Biggs <git@killbots.net>
2018-01-09 17:36:40 +0900

    Add lifecycle sample

---
+++ managed_greeter/include/managed_greeter/managed_greeter_component.hpp
@@ -0,0 +1,73 @@
+// Copyright 2018 Geoffrey Biggs
+
+#if !defined MANAGED_GREETER__MANAGED_GREETER_COMPONENT_HPP_
+#define MANAGED_GREETER__MANAGED_GREETER_COMPONENT_HPP_
+
+#if __cplusplus
+extern "C" {
+#endif
+
+// The below macros are taken from https://gcc.gnu.org/wiki/Visibility and from
+// demos/composition/include/composition/visibility_control.h at https://github.com/ros2/demos
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define MANAGED_GREETER_EXPORT __attribute__ ((dllexport))
+    #define MANAGED_GREETER_IMPORT __attribute__ ((dllimport))
+  #else
+    #define MANAGED_GREETER_EXPORT __declspec(dllexport)
+    #define MANAGED_GREETER_IMPORT __declspec(dllimport)
+  #endif
+  #ifdef MANAGED_GREETER_BUILDING_DLL
+    #define MANAGED_GREETER_PUBLIC MANAGED_GREETER_EXPORT
+  #else
+    #define MANAGED_GREETER_PUBLIC MANAGED_GREETER_IMPORT
+  #endif
+  #define MANAGED_GREETER_PUBLIC_TYPE MANAGED_GREETER_PUBLIC
+  #define MANAGED_GREETER_LOCAL
+#else
+  #define MANAGED_GREETER_EXPORT __attribute__ ((visibility("default")))
+  #define MANAGED_GREETER_IMPORT
+  #if __GNUC__ >= 4
+    #define MANAGED_GREETER_PUBLIC __attribute__ ((visibility("default")))
+    #define MANAGED_GREETER_LOCAL  __attribute__ ((visibility("hidden")))
+  #else
+    #define MANAGED_GREETER_PUBLIC
+    #define MANAGED_GREETER_LOCAL
+  #endif
+  #define MANAGED_GREETER_PUBLIC_TYPE
+#endif
+
+#if __cplusplus
+} // extern "C"
+#endif
+
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_lifecycle/lifecycle_node.hpp>
+#include <rclcpp_lifecycle/lifecycle_publisher.hpp>
+#include <greeter_custom_msg/msg/greeting.hpp>
+
+namespace managed_greeter
+{
+
+class Greeter : public rclcpp_lifecycle::LifecycleNode
+{
+public:
+  MANAGED_GREETER_PUBLIC
+  Greeter();
+
+private:
+  rclcpp_lifecycle::LifecyclePublisher<greeter_custom_msg::msg::Greeting>::SharedPtr pub_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  greeter_custom_msg::msg::Greeting::SharedPtr greeting_;
+
+  rcl_lifecycle_transition_key_t on_configure(const rclcpp_lifecycle::State &);
+  rcl_lifecycle_transition_key_t on_activate(const rclcpp_lifecycle::State &);
+  rcl_lifecycle_transition_key_t on_deactivate(const rclcpp_lifecycle::State &);
+  rcl_lifecycle_transition_key_t on_cleanup(const rclcpp_lifecycle::State &);
+
+  void broadcast_greeting();
+};
+
+} // namespace managed_greeter
+
+#endif // MANAGED_GREETER__MANAGED_GREETER_COMPONENT_HPP_
