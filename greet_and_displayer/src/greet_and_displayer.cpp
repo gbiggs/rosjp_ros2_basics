059af35eea39607975506763859c96546e5faafb
Geoffrey Biggs <git@killbots.net>
2018-01-12 15:58:15 +0900

    Add greet_and_displayer node

---
+++ greet_and_displayer/src/greet_and_displayer.cpp
@@ -0,0 +1,39 @@
+// Copyright 2018 Geoffrey Biggs, AIST
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <rclcpp/rclcpp.hpp>
+#include <memory>
+
+#include <greeter/greeter_component.hpp>
+#include <displayer/displayer_component.hpp>
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+
+  // タイマーコールバック、トピックコールバック等を行うexecutor
+  rclcpp::executors::SingleThreadedExecutor exec;
+
+  // Greeterコンポーネントノードのインスタンスを作成しexecutorに登録する
+  auto greeter = std::make_shared<greeter::Greeter>();
+  exec.add_node(greeter);
+  // Displayerコンポーネントノードのインスタンスを作成しexecutorに登録する
+  auto displayer = std::make_shared<displayer::Displayer>();
+  exec.add_node(displayer);
+
+  // Ctrl-Cが押されるまでexecutorを実行する
+  exec.spin();
+
+  rclcpp::shutdown();
+  return 0;
+}
